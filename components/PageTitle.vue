3ff0e9114f0a160d6da8445cbf5c07ca90a8ee90
PireJoseph <he201388@students.ephec.be>
2020-07-14 21:20:59 +0200

    feat(Layout): Create page title component  🏷

---
+++ components/PageTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          {{ title }}
+        </h1>
+        <h2 v-if="subTitle" class="subtitle">
+          {{ subTitle }}
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+}
+</script>
