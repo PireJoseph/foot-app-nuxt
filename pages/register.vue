b665600d169e65739f5322d551ad141fa4e28db7
PireJoseph <he201388@students.ephec.be>
2020-07-15 12:13:39 +0200

    feat(page): add validation to the register form ✅

--- pages/register.vue
+++ pages/register.vue
@@ -1,41 +1,127 @@
 <template>
   <div>
-    <PageTitle title="Register" />
+    <PageTitle title="Register" sub-title="Let's introduce yourself" />
     <section class="section">
-      <form>
-        <b-field label="Username">
-          <b-input v-model="registration.username" maxlength="30"></b-input>
-        </b-field>
-        <b-field label="Email">
-          <b-input v-model="registration.email" type="email" maxlength="30">
-          </b-input>
-        </b-field>
-        <b-field label="Password">
-          <b-input
-            v-model="registration.password"
-            type="password"
-            password-reveal
+      <ValidationObserver v-slot="{ invalid }">
+        <form>
+          <!-- username -->
+          <ValidationProvider v-slot="{ errors }" rules="min:8|max:30|required">
+            <b-field
+              label="Username"
+              :type="{
+                'is-danger': errors[0] || usernameErrorMesssage,
+                'is-success': errors.length,
+              }"
+              :message="errors[0] || usernameErrorMesssage"
+            >
+              <b-input
+                v-model="registration.username"
+                maxlength="30"
+                @input="usernameErrorMesssage = ''"
+              ></b-input>
+            </b-field>
+          </ValidationProvider>
+
+          <!-- Email field -->
+          <ValidationProvider
+            v-slot="{ errors }"
+            rules="email|min:8|max:30|required"
           >
-          </b-input>
-        </b-field>
-        <div class="buttons">
-          <b-button
-            tag="input"
-            value="Register"
-            type="is-success"
-            expanded
-            :loading="loading"
-            @click="register"
-          />
-        </div>
-      </form>
+            <b-field
+              label="Email"
+              :type="{
+                'is-danger': errors[0] || emailErrorMessage,
+                'is-success': errors.length,
+              }"
+              :message="errors[0] || emailErrorMessage"
+            >
+              <b-input
+                v-model="registration.email"
+                type="email"
+                maxlength="30"
+                @input="emailErrorMessage = ''"
+              >
+              </b-input>
+            </b-field>
+          </ValidationProvider>
+
+          <!-- Password field -->
+          <ValidationProvider
+            v-slot="{ errors }"
+            rules="min:8|max:30|required"
+            vid="password"
+          >
+            <b-field
+              label="Password"
+              :type="{
+                'is-danger': errors[0],
+                'is-success': errors.length,
+              }"
+              :message="errors[0]"
+            >
+              <b-input
+                v-model="registration.password"
+                type="password"
+                password-reveal
+                maxlength="30"
+              >
+              </b-input>
+            </b-field>
+          </ValidationProvider>
+
+          <!-- Confirm field -->
+          <ValidationProvider
+            v-slot="{ errors }"
+            rules="min:8|max:30|required|confirmed:password"
+          >
+            <b-field
+              label="Confirm password"
+              :type="{
+                'is-danger': errors[0],
+                'is-success': errors.length,
+              }"
+              :message="errors[0]"
+            >
+              <b-input
+                v-model="passwordConfirmation"
+                type="password"
+                password-reveal
+                maxlength="30"
+              >
+              </b-input>
+            </b-field>
+          </ValidationProvider>
+
+          <!-- register button -->
+          <div class="buttons">
+            <b-button
+              tag="input"
+              value="Register"
+              expanded
+              :type="{
+                'is-danger': invalid,
+                'is-success': !invalid,
+              }"
+              :disabled="invalid"
+              :loading="loading"
+              @click="register"
+            />
+          </div>
+        </form>
+      </ValidationObserver>
     </section>
   </div>
 </template>
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
+// import axios from 'axios'
+
 export default {
   auth: 'guest',
-  // page component definitions
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   data() {
     return {
       registration: {
@@ -43,7 +129,10 @@ export default {
         email: 'mail@provider.com',
         password: 'yourPassword',
       },
-      errorMesssage: String,
+      passwordConfirmation: 'yourPassword',
+      usernameErrorMesssage: '',
+      emailErrorMessage: '',
+      errorMessage: '',
       loading: false,
     }
   },
@@ -60,10 +149,26 @@ export default {
         const token = response.data.jwt
         this.$auth.setToken('local', `Bearer ${token}`)
         this.$auth.setUser(user)
-      } catch (err) {
-        this.errorMesssage = 'registration failed'
+      } catch (error) {
+        if (error.response.status === 400) {
+          switch (this.getMessagefromError(error).id) {
+            case 'Auth.form.error.username.taken':
+              this.usernameErrorMesssage = this.getMessagefromError(
+                error
+              ).message
+              break
+            case 'Auth.form.error.email.taken':
+              this.emailErrorMessage = this.getMessagefromError(error).message
+              break
+          }
+        } else {
+          this.errorMesssage = 'unknow registration problem :c'
+        }
       }
       this.loading = false
+    },
+    getMessagefromError(error) {
+      return error.response.data.message[0].messages[0]
     },
   },
 }
